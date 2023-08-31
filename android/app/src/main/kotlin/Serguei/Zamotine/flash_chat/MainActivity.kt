e698dd5609ba7e944ade01994482032e817fa987
szamotine <77281802+szamotine@users.noreply.github.com>
2023-08-30 18:16:16 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/Serguei/Zamotine/flash_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package Serguei.Zamotine.flash_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flash_chat/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const FlashChat());
+
+class FlashChat extends StatelessWidget {
+  const FlashChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.black54),
+        ),
+      ),
+      routes: {
+        WelcomeScreen.id: (context) => const WelcomeScreen(),
+        ChatScreen.id: (context) => const ChatScreen(),
+        RegistrationScreen.id: (context) => const RegistrationScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+      },
+      home: const WelcomeScreen(),
+      initialRoute: 'Welcome',
+    );
+  }
+}
